17e2ea62c0f4232c730afbfe60c85edd4ee18043
goalie2013 <gabriel.grinstein@gmail.com>
2025-01-30 23:58:52 -0800

    Update Home page to work live on AWS

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import profilePhoto from '../assets/profile-photo.jpg'
 
-const S3_BUCKET_URL = 'https://gg-portfolio-site.s3.us-west-1.amazonaws.com';
+const S3_BUCKET_URL = 'https://gg-portfolio-site.s3.us-west-1.amazonaws.com/assets/profile-photo-BkGm7-EV.jpg';
 
 const HomePage = () => {
   return (
@@ -47,7 +47,7 @@ const HomePage = () => {
               <div className="w-48 h-48 md:w-56 md:h-56 rounded-full overflow-hidden border-4 border-white shadow-xl">
                 <img
                   // src={profilePhoto}
-                  src={S3_BUCKET_URL}
+                  src={`${S3_BUCKET_URL}`}
                   alt="Profile Photo"
                   className="w-full h-full object-cover"
                 />
@@ -98,7 +98,7 @@ const HomePage = () => {
           <div className="flex justify-center space-x-12">
 
             {/* Github Icon */}
-            <a href="https://github.com/yourusername" className="text-gray-500 hover:text-gray-900 transition-colors">
+            <a href="https://github.com/goalie2013" className="text-gray-500 hover:text-gray-900 transition-colors">
               <span className="sr-only">GitHub</span>
               <svg className="h-10 w-10" fill="currentColor" viewBox="0 0 22 22">
                 <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
@@ -106,7 +106,7 @@ const HomePage = () => {
             </a>
 
             {/* LinkedIn Icon */}
-            <a href="https://linkedin.com/in/yourusername" className="text-gray-500 hover:text-blue-500 transition-colors">
+            <a href="https://www.linkedin.com/in/gabriel-grinstein" className="text-gray-500 hover:text-blue-500 transition-colors">
               <span className="sr-only">LinkedIn</span>
               <svg className="h-10 w-10" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
